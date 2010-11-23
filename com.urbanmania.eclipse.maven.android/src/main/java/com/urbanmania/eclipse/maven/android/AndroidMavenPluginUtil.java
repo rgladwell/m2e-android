1e9a0937dc890c67f73cb952e8ce51c16b6cb8a4
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2010-11-23 20:39:51 +0000

    Only configure projects with APK packaging type.

--- com.urbanmania.eclipse.maven.android/src/main/java/com/urbanmania/eclipse/maven/android/AndroidMavenPluginUtil.java
+++ com.urbanmania.eclipse.maven.android/src/main/java/com/urbanmania/eclipse/maven/android/AndroidMavenPluginUtil.java
@@ -9,9 +9,7 @@
 package com.urbanmania.eclipse.maven.android;
 
 import java.io.File;
-import java.util.List;
 
-import org.apache.maven.model.Plugin;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jdt.core.IJavaProject;
@@ -22,8 +20,7 @@ import com.android.ide.eclipse.adt.internal.project.ProjectHelper;
 
 public class AndroidMavenPluginUtil {
 
-	private static final String ANDROID_PLUGIN_GROUP_ID = "com.jayway.maven.plugins.android.generation2";
-	private static final String ANDROID_PLUGIN_ARTIFACT_ID = "maven-android-plugin";
+	private static final String ANDROID_PACKAGE_TYPE = "apk";
 
 	public final static File getApkFile(IProject project) throws JavaModelException {
 		IJavaProject javaProject = JavaCore.create(project);
@@ -32,15 +29,7 @@ public class AndroidMavenPluginUtil {
 	}
 
 	public static boolean isAndroidProject(MavenProject mavenProject) {
-		List<Plugin> plugins = mavenProject.getBuildPlugins();
-
-		for (Plugin plugin : plugins) {
-			if (ANDROID_PLUGIN_GROUP_ID.equals(plugin.getGroupId()) && ANDROID_PLUGIN_ARTIFACT_ID.equals(plugin.getArtifactId())) {
-				return true;
-			}
-		}
-
-		return false;
+		return mavenProject.getPackaging().equalsIgnoreCase(ANDROID_PACKAGE_TYPE);
 	}
 
 }
