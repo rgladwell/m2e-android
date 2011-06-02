cb239a0f87511afd7b7f6625046badddc919ff4c
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2011-06-02 12:54:11 +0000

    Tidy

--- com.googlecode.eclipse.m2e.android.test/src/com/googlecode/eclipse/m2e/android/test/LibraryAndroidMavenPluginTest.java
+++ com.googlecode.eclipse.m2e.android.test/src/com/googlecode/eclipse/m2e/android/test/LibraryAndroidMavenPluginTest.java
@@ -1,12 +1,7 @@
 package com.googlecode.eclipse.m2e.android.test;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
-import org.eclipse.jdt.core.IClasspathEntry;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
 
-import com.android.ide.eclipse.adt.AndroidConstants;
 import com.android.ide.eclipse.adt.internal.sdk.Sdk;
 
 public class LibraryAndroidMavenPluginTest extends AndroidMavenPluginTestCase {
