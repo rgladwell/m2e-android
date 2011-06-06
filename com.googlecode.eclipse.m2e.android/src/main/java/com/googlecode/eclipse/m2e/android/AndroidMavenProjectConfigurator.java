d5991ef36a870ccf03d591af26ccb0f7538e9812
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2011-06-06 16:58:44 +0000

    tidy

--- com.googlecode.eclipse.m2e.android/src/main/java/com/googlecode/eclipse/m2e/android/AndroidMavenProjectConfigurator.java
+++ com.googlecode.eclipse.m2e.android/src/main/java/com/googlecode/eclipse/m2e/android/AndroidMavenProjectConfigurator.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.maven.plugin.MojoExecution;
