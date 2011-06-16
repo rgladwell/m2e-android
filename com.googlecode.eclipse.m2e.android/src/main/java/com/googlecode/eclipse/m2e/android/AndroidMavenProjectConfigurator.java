4444fb43f0097dd674920b7ddc48cbe4fb230157
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2011-06-16 15:30:32 +0000

    [issue 23] Tidied code

--- com.googlecode.eclipse.m2e.android/src/main/java/com/googlecode/eclipse/m2e/android/AndroidMavenProjectConfigurator.java
+++ com.googlecode.eclipse.m2e.android/src/main/java/com/googlecode/eclipse/m2e/android/AndroidMavenProjectConfigurator.java
@@ -21,7 +21,6 @@ import java.util.List;
 import org.apache.maven.plugin.MojoExecution;
 import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
@@ -29,7 +28,6 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.m2e.core.internal.IMavenConstants;
@@ -55,59 +53,24 @@ public class AndroidMavenProjectConfigurator extends JavaProjectConfigurator imp
 	@Override
 	public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
 		super.configure(request, monitor);
+
 		ProjectType type = AndroidMavenPluginUtil.getAndroidProjectType(request.getMavenProject());
+		
 		if (type != null) {
 			IProject project = request.getProject();
+			ProjectHelper.fixProject(project);
 
 			if (!project.hasNature(AdtConstants.NATURE_DEFAULT)) {
 				addNature(project, AdtConstants.NATURE_DEFAULT, monitor);
 			}
 
-			// ensure APKBuilder build command is before maven build command 
-			IProjectDescription description = project.getDescription();
-			List<ICommand> buildCommands = Arrays.asList(description.getBuildSpec());
-			
-			Collections.sort(buildCommands, new Comparator<ICommand>() {
-				public int compare(ICommand command1, ICommand command2) {
-					if(IMavenConstants.BUILDER_ID.equals(command1.getBuilderName()) && APK_BUILDER_COMMAND_NAME.equals(command2.getBuilderName())) {
-						return 1;
-					} else if(APK_BUILDER_COMMAND_NAME.equals(command1.getBuilderName()) && IMavenConstants.BUILDER_ID.equals(command2.getBuilderName())) {
-						return -1;
-					}
-
-					return 0;
-				}
-			});
-
-			ICommand[] buildSpec = buildCommands.toArray(new ICommand[0]);
-			description.setBuildSpec(buildSpec);
-			project.setDescription(description, monitor);
+			orderBuilders(monitor, project);
 
 			ProjectState state = Sdk.getProjectState(project);
 			if(type == ProjectType.Library && !state.isLibrary()) {
-			    IFile defaultProperties = project.getFile("default.properties");
-			    BufferedWriter writer = null;
-			    try {
-			    	writer = new BufferedWriter( new FileWriter(new File(defaultProperties.getRawLocation().toOSString())) );
-			    	writer.newLine();
-					writer.append("android.library=true");
-				} catch (FileNotFoundException e) {
-					throw new CoreException(new Status(IStatus.ERROR, AndroidMavenPlugin.PLUGIN_ID, "cannot find default.propertie file", e));
-				} catch (IOException e) {
-					throw new CoreException(new Status(IStatus.ERROR, AndroidMavenPlugin.PLUGIN_ID, "cannot read default.propertie file", e));
-				} finally {
-					if(writer != null) {
-						try {
-							writer.close();
-						} catch (IOException e) {
-							throw new CoreException(new Status(IStatus.WARNING, AndroidMavenPlugin.PLUGIN_ID, "error closing file", e));
-						}
-					}
-				}
+			    convertToLibraryProject(project);
 				state.reloadProperties();
 			}
-			
-			ProjectHelper.fixProject(project);
 		}
 	}
 
@@ -130,5 +93,49 @@ public class AndroidMavenProjectConfigurator extends JavaProjectConfigurator imp
 	    	classpath.addSourceEntry(genPath, AndroidMavenPluginUtil.getAndroidClassesOutputFolder(javaProject), true);
 	    }
     }
+
+	private void orderBuilders(IProgressMonitor monitor, IProject project) throws CoreException {
+		// ensure APKBuilder build command is before maven build command 
+		IProjectDescription description = project.getDescription();
+		List<ICommand> buildCommands = Arrays.asList(description.getBuildSpec());
+		
+		Collections.sort(buildCommands, new Comparator<ICommand>() {
+			public int compare(ICommand command1, ICommand command2) {
+				if(IMavenConstants.BUILDER_ID.equals(command1.getBuilderName()) && APK_BUILDER_COMMAND_NAME.equals(command2.getBuilderName())) {
+					return 1;
+				} else if(APK_BUILDER_COMMAND_NAME.equals(command1.getBuilderName()) && IMavenConstants.BUILDER_ID.equals(command2.getBuilderName())) {
+					return -1;
+				}
+
+				return 0;
+			}
+		});
+
+		ICommand[] buildSpec = buildCommands.toArray(new ICommand[0]);
+		description.setBuildSpec(buildSpec);
+		project.setDescription(description, monitor);
+	}
+
+	private void convertToLibraryProject(IProject project) throws CoreException {
+		IFile defaultProperties = project.getFile("default.properties");
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter( new FileWriter(new File(defaultProperties.getRawLocation().toOSString())) );
+			writer.newLine();
+			writer.append("android.library=true");
+		} catch (FileNotFoundException e) {
+			throw new CoreException(new Status(IStatus.ERROR, AndroidMavenPlugin.PLUGIN_ID, "cannot find default.propertie file", e));
+		} catch (IOException e) {
+			throw new CoreException(new Status(IStatus.ERROR, AndroidMavenPlugin.PLUGIN_ID, "cannot read default.propertie file", e));
+		} finally {
+			if(writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					throw new CoreException(new Status(IStatus.WARNING, AndroidMavenPlugin.PLUGIN_ID, "error closing file", e));
+				}
+			}
+		}
+	}
 
 }
