0027723e040b049c72b4007ac1f4865173440473
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2012-02-12 15:32:22 +0000

    [task #8] Refactor: extract common project property modification code into abstract template class.

--- me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/configuration/ConvertLibraryProjectConfigurer.java
+++ me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/configuration/ConvertLibraryProjectConfigurer.java
@@ -1,41 +1,29 @@
 package me.gladwell.eclipse.m2e.android.configuration;
 
-import java.io.IOException;
 
 import me.gladwell.eclipse.m2e.android.model.AndroidProject;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IProgressMonitor;
 
 import com.android.ide.eclipse.adt.internal.sdk.ProjectState;
 import com.android.ide.eclipse.adt.internal.sdk.Sdk;
-import com.android.io.StreamException;
 import com.android.sdklib.internal.project.ProjectProperties;
 import com.android.sdklib.internal.project.ProjectPropertiesWorkingCopy;
 
-public class ConvertLibraryProjectConfigurer implements ProjectConfigurer {
+public class ConvertLibraryProjectConfigurer extends ProjectPropertiesConfigurer {
 
 	public boolean isConfigured(IProject project) {
 		ProjectState state = Sdk.getProjectState(project);
-		return !state.isLibrary();
+		return state.isLibrary();
 	}
 
 	public boolean isValid(AndroidProject androidProject) {
 		return androidProject.getType().equals(AndroidProject.Type.Library);
 	}
 
-	public void configure(IProject project, AndroidProject androidProject, IProgressMonitor monitor) {
-		try {
-			ProjectState state = Sdk.getProjectState(project);
-			ProjectPropertiesWorkingCopy workingCopy = state.getProperties().makeWorkingCopy();
-			workingCopy.setProperty(ProjectProperties.PROPERTY_LIBRARY, "true");
-			workingCopy.save();
-			state.reloadProperties();
-		} catch (IOException e) {
-			throw new ProjectConfigurationException(e);
-		} catch (StreamException e) {
-			throw new ProjectConfigurationException(e);
-		}
-		
+	@Override
+	protected void configureProperties(ProjectPropertiesWorkingCopy workingCopy) {
+		workingCopy.setProperty(ProjectProperties.PROPERTY_LIBRARY, "true");
 	}
+
 }
---
+++ me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/configuration/ProjectPropertiesConfigurer.java
@@ -0,0 +1,35 @@
+package me.gladwell.eclipse.m2e.android.configuration;
+
+import java.io.IOException;
+
+import me.gladwell.eclipse.m2e.android.model.AndroidProject;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.android.ide.eclipse.adt.internal.sdk.ProjectState;
+import com.android.ide.eclipse.adt.internal.sdk.Sdk;
+import com.android.io.StreamException;
+import com.android.sdklib.internal.project.ProjectProperties;
+import com.android.sdklib.internal.project.ProjectPropertiesWorkingCopy;
+
+public  abstract class ProjectPropertiesConfigurer implements ProjectConfigurer {
+
+	public void configure(IProject project, AndroidProject androidProject, IProgressMonitor monitor) {
+		try {
+			ProjectState state = Sdk.getProjectState(project);
+			ProjectPropertiesWorkingCopy workingCopy = state.getProperties().makeWorkingCopy();
+			configureProperties(workingCopy);
+			workingCopy.save();
+			state.reloadProperties();
+		} catch (IOException e) {
+			throw new ProjectConfigurationException(e);
+		} catch (StreamException e) {
+			throw new ProjectConfigurationException(e);
+		}
+		
+	}
+
+	protected abstract void configureProperties(ProjectPropertiesWorkingCopy workingCopy);
+
+}
