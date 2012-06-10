a177cee2275678df49251257f5793c3409cb9a33
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2012-06-10 13:34:27 +0100

    [task #8] Add: missing copyright notices.

--- me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/project/MavenDependency.java
+++ me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/project/MavenDependency.java
@@ -4,6 +4,14 @@ import static me.gladwell.eclipse.m2e.android.project.JaywayMavenAndroidProject.
 
 import org.apache.maven.artifact.Artifact;
 
+/*******************************************************************************
+ * Copyright (c) 2012 Ricardo Gladwell
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
 public class MavenDependency implements Dependency {
 
 	private Artifact artifact;
