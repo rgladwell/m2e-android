77f3eba88c4f63dd33ea75727f96b2c5641be547
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2012-08-14 13:01:04 +0100

    Refactor: switch to simpler Android Library test project for faster integration tests.

--- me.gladwell.eclipse.m2e.android.test/src/me/gladwell/eclipse/m2e/android/test/LibraryAndroidMavenPluginTest.java
+++ me.gladwell.eclipse.m2e.android.test/src/me/gladwell/eclipse/m2e/android/test/LibraryAndroidMavenPluginTest.java
@@ -12,7 +12,6 @@ import static com.android.ide.eclipse.adt.internal.sdk.Sdk.getProjectState;
 import static org.eclipse.m2e.core.MavenPlugin.getProjectConfigurationManager;
 
 import java.io.File;
-import java.io.IOException;
 
 import junit.framework.Assert;
 
@@ -20,16 +19,16 @@ import me.gladwell.eclipse.m2e.android.AndroidMavenPlugin;
 import me.gladwell.eclipse.m2e.android.configuration.ProjectConfigurationException;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 
 public class LibraryAndroidMavenPluginTest extends AndroidMavenPluginTestCase {
 
-    private static final String ANDROID_LIB_PROJECT_NAME = "apklib-project";
-
-    private IProject libraryProject;
+    private static final String ANDROID_LIB_PROJECT_NAME = "android-library";
+    private static final String TEST_PROJECT_WITH_APKLIB_DEPS = "test-project-apklib-deps";
 
 	@Override
 	protected void setUp() throws Exception {
@@ -65,52 +64,49 @@ public class LibraryAndroidMavenPluginTest extends AndroidMavenPluginTestCase {
 
 	public void testConfigureAddsWorkspaceLibraryProjectToProjectProperties() throws Exception {
 		IProject libraryProject = importAndroidProject(ANDROID_LIB_PROJECT_NAME);
-		IProject project = importAndroidProject("test-project-apklib-deps");
+		IProject project = importAndroidProject(TEST_PROJECT_WITH_APKLIB_DEPS);
 
 		assertTrue(getProjectState(project).getFullLibraryProjects().contains(libraryProject));
 
 		try {
-		    deleteProject("test-project-apklib-deps");
+		    deleteProject(TEST_PROJECT_WITH_APKLIB_DEPS);
 		} catch(Throwable t) {
-		    
+            t.printStackTrace();
 		}
 	}
 
 	public void testConfigureAddsErrorForNonExistentLibraryProject() throws Exception {
-		IProject project = importAndroidProject("test-project-apklib-deps");
+		IProject project = importAndroidProject(TEST_PROJECT_WITH_APKLIB_DEPS);
 
 		assertErrorMarker(project, AndroidMavenPlugin.APKLIB_ERROR_TYPE);
 
         try {
-            deleteProject("test-project-apklib-deps");
+            deleteProject(TEST_PROJECT_WITH_APKLIB_DEPS);
         } catch(Throwable t) {
-            
+            t.printStackTrace();
         }
 	}
 
-	public void testConfigureWithAClosedProjectInTheWorkspace() throws Exception {
-		
-		IProject javaProject = importAndroidProject("closed-java-project");
-		IJavaProject closedJavaProject = JavaCore.create(javaProject);
-		closedJavaProject.close();
-		javaProject.close(null);
+	public void testConfigureWithClosedProject() throws Exception {
+		IProject closedProject = importAndroidProject("simple-project");
+		closedProject.close(monitor);
+
 		try{
-			IProject project = importAndroidProject("test-project-apklib-deps");
-			
+			IProject project = importAndroidProject(TEST_PROJECT_WITH_APKLIB_DEPS);
 		} catch (CoreException ex){
 			if (ex.getCause() instanceof ProjectConfigurationException){
-				Assert.fail("Access denied: M2E is trying to access a closed project");
+				fail("Access denied: M2E is trying to access a closed project");
 			}
 		} finally {
 		    try {
-		        deleteProject("closed-java-project");
+		        deleteProject("simple-project");
             } catch(Throwable t) {
-                
+                t.printStackTrace();
             }
             try {
-                deleteProject("test-project-apklib-deps");
+                deleteProject(TEST_PROJECT_WITH_APKLIB_DEPS);
             } catch(Throwable t) {
-                
+                t.printStackTrace();
             }
 		}
 	}
@@ -129,44 +125,45 @@ public class LibraryAndroidMavenPluginTest extends AndroidMavenPluginTestCase {
 	}
 
 	public void testConfigureClearsOldErrors() throws Exception {
-		IProject project = importAndroidProject("test-project-apklib-deps");
+		IProject project = importAndroidProject(TEST_PROJECT_WITH_APKLIB_DEPS);
 		importAndroidProject(ANDROID_LIB_PROJECT_NAME);
 		getProjectConfigurationManager().updateProjectConfiguration(project, monitor);
 
 		assertNoErrors(project);
 
         try {
-            deleteProject("test-project-apklib-deps");
+            deleteProject(TEST_PROJECT_WITH_APKLIB_DEPS);
         } catch(Throwable t) {
-            
+            t.printStackTrace();
         }
 	}
 
     public void testConfigureAddsWorkspaceLibraryInSubfdoler() throws Exception {
         File subfolder = new File(workspace.getRoot().getLocation().toFile(), "subfolder");
         IProject libraryProject = importAndroidProject(ANDROID_LIB_PROJECT_NAME, subfolder);
-        IProject project = importAndroidProject("test-project-apklib-deps");
+        IProject project = importAndroidProject(TEST_PROJECT_WITH_APKLIB_DEPS);
 
         assertTrue(getProjectState(project).getFullLibraryProjects().contains(libraryProject));
 
         try {
-            deleteProject("test-project-apklib-deps");
+            deleteProject(TEST_PROJECT_WITH_APKLIB_DEPS);
         } catch(Throwable t) {
-            
+            t.printStackTrace();
         }
     }
 
     public void testConfigureAddsWorkspaceLibraryOutsideWorkspaceFolder() throws Exception {
         File temp = createTempFolder();
         IProject libraryProject = importAndroidProject(ANDROID_LIB_PROJECT_NAME, temp);
-        IProject project = importAndroidProject("test-project-apklib-deps");
+        IProject project = importAndroidProject(TEST_PROJECT_WITH_APKLIB_DEPS);
 
         assertTrue(getProjectState(project).getFullLibraryProjects().contains(libraryProject));
 
         try {
-            deleteProject("test-project-apklib-deps");
+            deleteProject(TEST_PROJECT_WITH_APKLIB_DEPS);
         } catch(Throwable t) {
-            
+            t.printStackTrace();
         }
     }
+
 }
