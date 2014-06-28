7ea516459de61dd67de5d60ed7a280a78a5f5d2f
Ricardo Gladwell <ricardo@gladwell.me>
2014-06-28 16:43:19 +0100

    Upgraded build for ADT version 23.
    Upgraded Maven and Travis builds for the latest ADT version 23.0.1. This
    included a breaking change where the feature IDs had been modified.
    
    Due to problems with ADT version 23 this release was ignored.
    
    https://github.com/rgladwell/m2e-android/issues/226

--- travis.sh
+++ travis.sh
@@ -1,8 +1,8 @@
 set -e
 set -x
 
-wget http://dl.google.com/android/android-sdk_r22.6.2-linux.tgz
-tar -zxf android-sdk_r22.6.2-linux.tgz
+wget http://dl.google.com/android/android-sdk_r23.0.1-linux.tgz
+tar -zxf android-sdk_r23.0.1-linux.tgz
 wget http://apache.mirror.anlx.net/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz
 tar -zxf apache-maven-3.2.1-bin.tar.gz
 export ANDROID_HOME=$PWD/android-sdk-linux
@@ -14,7 +14,7 @@ sudo apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true |  sudo debconf-set-selections
 echo yes | sudo apt-get install --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch xvfb git oracle-java7-installer
-echo yes | android update sdk --filter platform-tools,build-tools-19.1.0,android-18,addon-google_apis-google-18,android-10,android-7,extra-android-support,sysimg-16 --no-ui --force
+echo yes | android update sdk --filter platform-tools,build-tools-20.0.0,android-18,addon-google_apis-google-18,android-10,android-7,extra-android-support,sysimg-16 --no-ui --force
 git clone https://github.com/mosabua/maven-android-sdk-deployer.git
 cd maven-android-sdk-deployer
 mvn install -P 4.3
