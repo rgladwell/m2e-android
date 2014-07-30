a1cbde0e1ef1c22619852054420fce1c14c94784
WonderCsabo <kozakcsabi@gmail.com>
2014-07-30 13:14:15 +0200

    Added source path provider for JUnit launch configurations
    The sources for non-runtime dependencies are already attached
    automatically, but still not available in JUnit test debugging. This is
    because the source lookup does not contain the non-runtime dependency
    entries, only the run-time ones. This commit fixes this issue by adding
    a new source path provider which decorates the standard m2e one, and
    adds non-runtime dependencies too, to the JUnit source lookup path.

---
+++ me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/JUnitSourcepathProvider.java
@@ -0,0 +1,66 @@
+package me.gladwell.eclipse.m2e.android;
+
+import static java.util.Arrays.asList;
+import static org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME;
+import static org.eclipse.jdt.launching.JavaRuntime.newArchiveRuntimeClasspathEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.gladwell.eclipse.m2e.android.project.AndroidProjectFactory;
+import me.gladwell.eclipse.m2e.android.project.Dependency;
+import me.gladwell.eclipse.m2e.android.project.EclipseAndroidProject;
+import me.gladwell.eclipse.m2e.android.project.MavenAndroidProject;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
+import org.eclipse.jdt.launching.IRuntimeClasspathProvider;
+
+import com.google.inject.Inject;
+
+public class JUnitSourcepathProvider implements IRuntimeClasspathProvider {
+    
+    private final IRuntimeClasspathProvider classpathProvider;
+    private final IWorkspace workspace;
+    private final AndroidProjectFactory<EclipseAndroidProject, IProject> factory;
+    private final AndroidProjectFactory<MavenAndroidProject, EclipseAndroidProject> factory2;
+    
+    @Inject
+    public JUnitSourcepathProvider(@MavenSource IRuntimeClasspathProvider classpathProvider, IWorkspace workspace,
+            AndroidProjectFactory<EclipseAndroidProject, IProject> factory,
+            AndroidProjectFactory<MavenAndroidProject, EclipseAndroidProject> factory2) {
+        this.classpathProvider = classpathProvider;
+        this.workspace = workspace;
+        this.factory = factory;
+        this.factory2 = factory2;
+    }
+
+    public IRuntimeClasspathEntry[] computeUnresolvedClasspath(ILaunchConfiguration configuration) throws CoreException {
+        List<IRuntimeClasspathEntry> classpath = new ArrayList<IRuntimeClasspathEntry>(
+                asList(classpathProvider.computeUnresolvedClasspath(configuration)));
+        
+        addNonRuntimeDependencies(configuration, classpath);
+        
+        return classpath.toArray(new IRuntimeClasspathEntry[classpath.size()]);
+    }
+    
+    private void addNonRuntimeDependencies(ILaunchConfiguration config, List<IRuntimeClasspathEntry> classpath)
+            throws CoreException {
+        IProject project = workspace.getRoot().getProject(config.getAttribute(ATTR_PROJECT_NAME, (String) null));
+        MavenAndroidProject androidProject = factory2.createAndroidProject(factory.createAndroidProject(project));
+        
+        for (Dependency dependency : androidProject.getNonRuntimeDependencies()) {
+            classpath.add(newArchiveRuntimeClasspathEntry(new Path(dependency.getPath())));
+        }
+    }
+
+    public IRuntimeClasspathEntry[] resolveClasspath(IRuntimeClasspathEntry[] entries,
+            ILaunchConfiguration configuration) throws CoreException {
+        return classpathProvider.resolveClasspath(entries, configuration);
+    }
+
+}
