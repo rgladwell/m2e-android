a758b15d2b3e945b0b3f347ae268ce6943b88107
WonderCsabo <kozakcsabi@gmail.com>
2014-10-06 21:33:07 +0200

    Compatibility with android-maven-plugin-4
    Since version 4.0.0 the android-maven-plugin uses the gradle / Android
    Studio folder layout. This means AndroidManifest.xml is at
    src/main/AndroidManifest.xml, res is at src/main/res and assets is at
    src/main/assets by default. We already support non-ADT folder layout by
    using Eclipse resource links to point to the actual resource. The only
    thing this commit is changing that we are now reading default values
    from the android-maven-plugin parameters, so we can link to the default
    structure even if the user do not explicitely define these properties.
    
    https://github.com/rgladwell/m2e-android/issues/266

--- me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/project/ProjectModule.java
+++ me.gladwell.eclipse.m2e.android/src/main/java/me/gladwell/eclipse/m2e/android/project/ProjectModule.java
@@ -2,6 +2,8 @@ package me.gladwell.eclipse.m2e.android.project;
 
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.embedder.IMaven;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
@@ -15,6 +17,8 @@ public class ProjectModule extends AbstractModule {
         bind(new TypeLiteral<AndroidProjectFactory<MavenAndroidProject, MavenProject>>(){}).to(MavenAndroidProjectFactory.class);
         bind(new TypeLiteral<AndroidProjectFactory<EclipseAndroidProject, IProject>>(){}).to(EclipseAndroidProjectFactory.class);
         bind(new TypeLiteral<AndroidProjectFactory<MavenAndroidProject, EclipseAndroidProject>>(){}).to(MavenToEclipseAndroidProjectConverter.class);
+    
+        bind(IMaven.class).toInstance(MavenPlugin.getMaven());
     }
 
 }
